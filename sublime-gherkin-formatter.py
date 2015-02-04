bbc7e35fdfe3d650ad685c37097472c24ce5f01e
Mark Battersby <mark@mojotech.com>
2015-02-04 09:03:12 -0500

    [touchup] Only enable formatting in Gherkin files.

--- sublime-gherkin-formatter.py
+++ sublime-gherkin-formatter.py
@@ -6,8 +6,18 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from lib.formatter import ViewFormatter
 
+def is_gherkin(view=None):
+  if view is None:
+    view = sublime.active_window().active_view()
+  if 'text.gherkin' in view.scope_name(0):
+    return True
+  if os.path.splitext(view.file_name())[1] == '.feature':
+    return True
+  return False
 
 class FormatGherkinCommand(sublime_plugin.TextCommand):
+  def is_enabled(self):
+    return is_gherkin(self.view)
 
   def run(self, edit):
     ViewFormatter(sublime, self.view).format_view(edit)
